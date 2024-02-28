9112c23299449190c5c3b35f97791cd47589cb10
jurko <50539591+JurkoDev@users.noreply.github.com>
2024-02-28 20:50:44 +0100

    villager add frost melon seeds trade

---
+++ kubejs/server_scripts/villagers.js
@@ -0,0 +1,5 @@
+MoreJSEvents.villagerTrades((event) => {
+
+    // https://discord.com/channels/649590685829300245/1212204498357981194/1212476236433985587
+    event.addTrade("farmer", "5", [TradeItem.of("minecraft:emerald", 12, 20)], "thermal:frost_melon_seeds").maxUses(4).villagerExperience(0);
+});
